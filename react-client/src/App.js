b3540aecd0d5b8c6495fb9a7ef28e6c2f24aea77
Rikesh <rikesh.shrestha@citytech.global>
2019-10-16 17:52:51 +0545

    refactoring

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import socketIOClient from "socket.io-client";
